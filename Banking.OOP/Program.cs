19d9ae2cc4767449856e53e43d174b5569677a2e
Marco Gea <69200078+iwtapcgmr@users.noreply.github.com>
2024-07-29 21:31:17 -0300

    feat: código procedural

--- Banking.OOP/Program.cs
+++ Banking.OOP/Program.cs
@@ -3,6 +3,9 @@
 	public int Id { get; set; }
 	public string Titular { get; set; }
 	public string Documento { get; set; }
+	public string Banco { get; set; }
+	public string Agencia { get; set; }
+	public string Numero { get; set; }
 	public decimal LimiteDiario { get; set; }
 	public decimal Saldo { get; set; }
 
@@ -25,6 +28,13 @@ public class TransferirDinheiroService
 		decimal valor,
 		DateTime timestamp)
 	{
+		if (contaOrigem.Banco == contaDestino.Banco &&
+			contaOrigem.Agencia == contaDestino.Agencia &&
+			contaOrigem.Numero == contaDestino.Numero)
+		{
+			throw new ArgumentException("Uma transferência só pode ser realizada entre contas diferentes.", nameof(contaDestino));
+		}
+
 		if (valor <= 0)
 			throw new ArgumentException("Valor deve ser maior que 0.", nameof(valor));
 
@@ -55,5 +65,55 @@ public class TransferirDinheiroService
 
 		contaDestino.Saldo += valor;
 		contaDestino.Movimentacoes.Add(movimentacao);
+	}
+}
+
+public class DepositarDinheiroService
+{
+	public void Depositar(
+		ContaBancaria contaDestino,
+		decimal valor,
+		DateTime timestamp)
+	{
+		if (valor <= 0)
+			throw new ArgumentException("Valor deve ser maior que 0.", nameof(valor));
+
+		Movimentacao movimentacao = new()
+		{
+			Destino = contaDestino,
+			Valor = valor,
+			Timestamp = timestamp
+		};
+
+		contaDestino.Saldo += valor;
+		contaDestino.Movimentacoes.Add(movimentacao);
+	}
+}
+
+public class SacarDinheiroService
+{
+	public void Sacar(
+		ContaBancaria contaOrigem,
+		decimal valor,
+		DateTime timestamp)
+	{
+		if (valor <= 0)
+			throw new ArgumentException("Valor deve ser maior que 0.", nameof(valor));
+
+		if (valor > 1000)
+			throw new ArgumentException("Valor deve ser menor ou igual a 1000.", nameof(valor));
+
+		if (contaOrigem.Saldo < valor)
+			throw new InvalidOperationException("Saldo insuficiente.");
+
+		Movimentacao movimentacao = new()
+		{
+			Origem = contaOrigem,
+			Valor = valor,
+			Timestamp = timestamp
+		};
+
+		contaOrigem.Saldo -= valor;
+		contaOrigem.Movimentacoes.Add(movimentacao);
 	}
 }
